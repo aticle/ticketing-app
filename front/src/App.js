7f2668214928710c2e34ee8789e448d2fbf526e7
Andreea TICLE <aticle@p5ro-port715.local>
2018-11-20 10:35:14 +0200

    Refactor: App doesn't need to be a react component.

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React from 'react';
 
 import './App.css';
 
@@ -7,16 +7,12 @@ import MenuAppBar from './components/MenuAppBar/MenuAppBar';
 import TicketsList from './components/TicketsList/TicketsList';
 import CreateTicket from './containers/CreateTicket/CreateTicket';
 
-class App extends Component<void, void> {
-  render() {
-    return (
-      <div className="App">
-        <MenuAppBar></MenuAppBar>
-        <CreateTicket />
-        <TicketsList />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <MenuAppBar></MenuAppBar>
+    <CreateTicket />
+    <TicketsList />
+  </div>
+);
 
 export default App;
